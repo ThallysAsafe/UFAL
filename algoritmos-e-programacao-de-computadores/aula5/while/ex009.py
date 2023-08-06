5859a3171791b912216d7c36957b77761929bfe8
Thallys Asafe <thallysribeiro37@gmail.com>
2023-08-06 17:42:11 -0300

    alteration of python exercises

--- algoritmos-e-programacao-de-computadores/aula5/while/ex009.py
+++ algoritmos-e-programacao-de-computadores/aula5/while/ex009.py
@@ -4,23 +4,28 @@ diferença = []
 valorN = 0
 menor = 0
 cont = 0
-while True:
-    conjunto.append(int(input('Digite um numero inteiro: ')))
-    r = str(input('Quer Continuar? [S/N]'))
-    if r in 'Nn':
-        break
-valorN = float(input('Digite o valor N: '))
-for n in conjunto:
-    diferença.append(valorN-n)
-    if (valorN-n) == 0:
-        menor = n
-        cont += 1
-for pos, proximo in enumerate(diferença):
-    if cont >= 1:
-        break
-    elif pos == 0:
-        menor = conjunto[pos]
-    elif proximo >= 0 and proximo <= menor:
-        menor = conjunto[pos]
-print(conjunto)
-print(f'O numero mais proximo de {valorN} é: {menor}')
+qnt = int(input('Digite quantos elementos deseja adicionar a um conjunto: '))
+if qnt > 0:
+    while qnt != 0:
+        conjunto.append(int(input('Digite um numero inteiro: ')))
+        qnt -= 1
+    valorN = float(input('Digite o valor N: '))
+    for n in conjunto:
+        diferença.append(n-valorN)
+        if (n-valorN) == 0:
+            menor = n
+            cont += 1
+    for posição, diferença_numero in enumerate(diferença):
+        if cont >= 1:
+            break
+        if diferença_numero < 0:
+            diferença_numero = diferença_numero * -1
+        if posição == 0:
+            menor = diferença_numero
+            mais_proximo = conjunto[posição]
+        elif  diferença_numero >= 0 and diferença_numero < menor:
+            mais_proximo = conjunto[posição]
+            menor = diferença_numero
+    print(f'O numero mais proximo de {valorN} é: {mais_proximo}')
+else: 
+    print('Quantidade Inválida!')
--- hx0011.py
+++ hx0011.py
@@ -1,8 +1,11 @@
 num = int(input())
-if num >= 1 and num <= 40:
+if (num >= 1 and num <= 40):
     for coluna in range(1, num+1):
         for linha in range(1, coluna+1):
-            print(linha, end='')
+            if (linha<coluna):
+                print(linha, end=' ')
+            else:
+                print(linha, end='')
         print()
     
         

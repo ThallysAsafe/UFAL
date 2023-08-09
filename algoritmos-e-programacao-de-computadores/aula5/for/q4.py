d54533266cca2d49e5607ec8c6a49d33dbc2a1ad
Thallys Asafe <thallysribeiro37@gmail.com>
2023-08-09 10:02:37 -0300

    Fixing Python Exercises

--- algoritmos-e-programacao-de-computadores/aula5/for/q4.py
+++ algoritmos-e-programacao-de-computadores/aula5/for/q4.py
@@ -3,18 +3,20 @@ imcs = []
 nomes = []
 pesos = []
 alturas = []
-for c in range(1,4):
+x = 0  
+qnt = int(input('quantas pessoas voce quer adicionar a lista? '))
+for c in range(1,qnt+1):
     nome = (str(input(f'Digite o nome da {c}ª pessoa: ')))
     peso = (float(input(f'Digite o peso da {c}ª pessoa, em Kg: ')))
     altura = (float(input(f'Digite a altura da {c}ª pessoa, em Metros: ')))
-    if peso < 0 and altura < 0:
-        print('Dados inválidos, Tente Novamente!')
-        c -= 1
+    if peso < 0 or altura < 0:
+        print('Dados inválidos, Valor não adicionado!')     
     else:
+        x += 1
         nomes.append(nome)
         pesos.append(peso)
         alturas.append(altura)
-        imc = peso // altura**2
+        imc = peso // (altura**2)
         imcs.append(imc)
-for c in range(0,3):
+for c in range(x):
     print(f'{nomes[c]} e {imcs[c]}')
--- algoritmos-e-programacao-de-computadores/aula5/for/q5.py
+++ algoritmos-e-programacao-de-computadores/aula5/for/q5.py
@@ -2,18 +2,20 @@
 nomes = []
 salarios = []
 impostos = []
-for c in range(0,3):
+x = 0
+qnt = int(input('Quantos funcionarios deseja adicionar a lista? '))
+for c in range(qnt):
     nome = str(input('Digite o nome do funcionário: '))
     salario = int(input('Digite o salario dele: R$'))
     if nome == int or salario <= 0:
-        print('Entradas inválidas, Tente Novamente!')
-        c -= 1
+        print('Entradas inválidas, não foram adicionados a lista!')
     else:
+        x += 1
         imposto = 0
         nomes.append(nome)
         salarios.append(salario)
         imposto = imposto + salario*0.1
         impostos.append(imposto)
-for i in range(0,3):
+for i in range(x):
     print(f'{nomes[i]}, com salario de R${salarios[i]}, pagará o imposto de R${impostos[i]}')
 

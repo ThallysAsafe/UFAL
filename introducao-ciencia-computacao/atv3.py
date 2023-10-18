3c49b7501542fa214a2a1ac474efc53937149854
Thallys Asafe <thallysribeiro37@gmail.com>
2023-10-18 16:43:37 -0300

    resolvendo o Bug

--- introducao-ciencia-computacao/atv3.py
+++ introducao-ciencia-computacao/atv3.py
@@ -33,106 +33,142 @@
         # ◦ Podemos limitar as bases possíveis de 2 a 36. O limite até a base 36 é para assumir
         # que nos limitaremos aos números e letras do alfabeto, ok?
 
-def inicio():
+import tkinter
+def inicio(alfabeto):
     numero = input('Digite o numero: ').upper()
     base_origem = int(input('Insira a base de origem: [2 a 36]'))
     base_destino = int(input('Insira qual a base destino: [2 a 36]'))
-    resultado = converter_base(numero, base_origem,base_destino)
-    print(f'{numero} escrito na base {base_origem} é equivalente a {resultado} na base {base_destino}')
+    condição = False
+    if '.' in numero: # Verificador decimal
+        numero, decimal = numero.split('.')
+        for num in decimal:
+            if str(num).isalpha():
+                print(alfabeto[num])
+                if int(alfabeto[num]) >= base_origem:
+                    condição = True
+                    break
+            else:
+                if int(num) >= base_origem:
+                    condição = True
+                    break
+    for num in numero:
+        if str(num).isalpha():
+            print(alfabeto[num])
+            if int(alfabeto[num]) >= base_origem:
+                condição = True
+                break
+        else:
+            if int(num) >= base_origem:
+                condição = True
+                break
+    if condição == False:
+        resultado = converter_base(numero, base_origem,base_destino, alfabeto)
+        print(f'{numero} escrito na base {base_origem} é equivalente a {resultado} na base {base_destino}')
+    else:
+        print('Não foi possivel calcular este numero, tente novamente mais tarde!')
+        print()
 
-def converter_base(numero, base_origem,base_destino):
+def converter_base(numero, base_origem,base_destino, alfabeto):
 
     if base_origem != 10:
-        numero = converter_para_base10(numero, base_origem)
-        if base_destino != 10:
-            resultado = converter_da_base10(numero, base_destino)
+        numero = converter_para_base10(numero, base_origem, alfabeto)
+        if base_destino != 10 and numero != None:
+            resultado = converter_da_base10(numero, base_destino, alfabeto)
             return resultado
     elif base_destino != 10:
-        resultado = converter_da_base10(numero, base_destino)
+        resultado = converter_da_base10(numero, base_destino, alfabeto)
         return resultado
     return numero
 
-def converter_da_base10(numero, base_destino):
-    alfabeto = {"A": 10,"B": 11,"C": 12,"D": 13,"E": 14,"F": 15,"G":16, "H": 17,"I": 18,"J": 19,
-                "K": 20,"L": 21,"M": 22,"N": 23,"O": 24,"P": 25,"Q": 26,"R": 27,"S": 28,"T": 29,
-                "U": 30,"V": 31,"W": 32,"X": 33,"Y": 34,"Z": 35}
+def converter_da_base10(numero, base_destino, alfabeto):
     resto_divisão = []
     resultado = ''
-    numero, decimal = numero.split('.')
-    numero = int(numero)
-    tamanho = len(decimal)
-    decimal = int(decimal)
-    decimal = decimal / (10**tamanho)
+    verifica = False
     decimais = []
-    J = 0
     con = False
-    while decimal > 0 and len(decimais) < 22:# Mostrar a quantidade de decimais 
-        
-        if decimal > 0 :
-            con = True
-            decimal = decimal * base_destino
-            decimal = str(decimal)
-            inteiro, decimal = decimal.split('.')
-            tamanho = len(decimal)  
-            decimal = int(decimal)
-            decimal = decimal / (10**tamanho)
-            inteiro = int(inteiro)
-            if inteiro >= 0:
-                decimais.append(inteiro)
-    while base_destino <= numero:
-        resto = numero % base_destino
-        numero = numero // base_destino
-        resto_divisão.insert(0, resto)
-    resto_divisão.insert(0, numero)
-    for i in range(len(resto_divisão)):
-        cond = False
-        for letras in alfabeto.keys():
-            if alfabeto[letras] == resto_divisão[i]:
-                resultado += f'{letras}'
-                cond = True
-            elif alfabeto[letras] != resto_divisão[i] and alfabeto[letras] == 35 and cond == False:
-                resultado += f'{resto_divisão[i]}'
-    if con == True:
-        resultado += '.'
-    for i in range(len(decimais)): # Mostrar a quantidade de decimais 
-        cond = False
-        decimal = int(decimais[i])
-        for letras in alfabeto.keys():
-            if alfabeto[letras] == decimal:
-                resultado += f'{letras}'
-                cond = True
-            elif alfabeto[letras] != decimais[i] and alfabeto[letras] == 35 and cond == False:
-                resultado += f'{decimais[i]}'
-    return resultado
-    
-def converter_para_base10(numero, base_origem):
-    alfabeto = {"A": 10,"B": 11,"C": 12,"D": 13,"E": 14,"F": 15,"G":16, "H": 17,"I": 18,"J": 19,
-                "K": 20,"L": 21,"M": 22,"N": 23,"O": 24,"P": 25,"Q": 26,"R": 27,"S": 28,"T": 29,
-                "U": 30,"V": 31,"W": 32,"X": 33,"Y": 34,"Z": 35}
+    k = 0
+    condição = False
+    if condição == False:
+        while decimal > 0 and len(decimais) < 22:# Mostrar a quantidade de decimais 
+            if decimal > 0 :
+                con = True
+                decimal = str(decimal)
+                inteiro, decimal = decimal.split('.')
+                tamanho = len(decimal)  
+                decimal = int(decimal)
+                decimal = (decimal / (10**tamanho))
+                decimal = decimal * base_destino
+                inteiro = int(inteiro)
+                if k == 0:
+                    numero = inteiro    
+                    k += 1
+                elif inteiro >= 0:
+                    decimais.append(inteiro)
+                    verifica = True
+        while base_destino <= numero:
+            resto = numero % base_destino
+            numero = numero // base_destino
+            resto_divisão.insert(0, resto)
+        resto_divisão.insert(0, numero)
+        for i in range(len(resto_divisão)):
+            cond = False
+            for letras in alfabeto.keys():
+                if alfabeto[letras] == resto_divisão[i]:
+                    resultado += f'{letras}'
+                    cond = True
+                elif alfabeto[letras] != resto_divisão[i] and alfabeto[letras] == 35 and cond == False:
+                    resultado += f'{resto_divisão[i]}'
+        if verifica == True:
+            if con == True:
+                resultado += '.'
+            for i in range(len(decimais)): # Mostrar a quantidade de decimais 
+                cond = False
+                decimal = int(decimais[i])
+                for letras in alfabeto.keys():
+                    if alfabeto[letras] == decimal:
+                        resultado += f'{letras}'
+                        cond = True
+                    elif alfabeto[letras] != decimais[i] and alfabeto[letras] == 35 and cond == False:
+                        resultado += f'{decimais[i]}'
+        return resultado
+    else:
+        print('Não foi possivel calcular este numero, tente novamente mais tarde!')
+        print()
+def converter_para_base10(numero, base_origem, alfabeto):
     resultado = 0
     i = 0
-    numero, decimal = numero.split('.')
     numero = str(numero)
-    tamanho = len(decimal)
-    j = 0
-    p = -1
-    somaDecimal = 0
-    print(decimal)
-    while j < tamanho:
-        if alfabeto[decimal[j]] > 0:
-            somaDecimal += ((alfabeto[decimal[j]]) * (base_origem**p))
+    verifica = numero
+    if '.' in numero:
+        numero, decimal = numero.split('.')
+        numero = str(numero)
+        tamanho = len(decimal)
+        j = 0
+        p = -1
+        somaDecimal = 0
+        while j < tamanho:
+            if decimal[j] in alfabeto and alfabeto[decimal[j]] > 0:
+                somaDecimal += ((alfabeto[decimal[j]]) * (base_origem**p))
+                
+            else:
+                somaDecimal += (int(decimal[j]) * (base_origem**p))
             p -= 1
-        else:
-            somaDecimal += (int(decimal[j]) * (base_origem**p))
-            p -= 1
-        j += 1
+            j += 1
     while i < len(numero):
         if numero[i].isalpha():
             resultado += (base_origem**(len(numero)-i-1)) * alfabeto[numero[i]]
+
         else:
             number = int(numero[i])
             resultado += number * (base_origem**(len(numero)-i-1))
         i += 1
-    resultado += somaDecimal
+    
+    if '.' in verifica:
+        resultado += somaDecimal
+        resultado = str(resultado)
+    resultado = str(resultado)    
     return resultado
-inicio()
+alfabeto = {"A": 10,"B": 11,"C": 12,"D": 13,"E": 14,"F": 15,"G": 16,"H": 17,"I": 18,"J": 19,
+                "K": 20,"L": 21,"M": 22,"N": 23,"O": 24,"P": 25,"Q": 26,"R": 27,"S": 28,"T": 29,
+                "U": 30,"V": 31,"W": 32,"X": 33,"Y": 34,"Z": 35}
+inicio(alfabeto)
--- introducao-ciencia-computacao/tst.py
+++ introducao-ciencia-computacao/tst.py
@@ -1,59 +1,6 @@
-import pygame
-from pygame.locals import *
+inteiro = '456'
+decimal = '89'
 
-pygame.init()
-
-# Configurações da janela
-largura, altura = 800, 600
-janela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption("Caixa de Perguntas e Respostas")
-
-# Cores
-branco = (255, 255, 255)
-preto = (0, 0, 0)
-
-# Fonte
-fonte = pygame.font.Font(None, 32)
-
-# Variáveis para perguntas e respostas
-pergunta = ""
-resposta = ""
-input_ativo = False
-
-executando = True
-while executando:
-    for evento in pygame.event.get():
-        if evento.type == QUIT:
-            executando = False
-        elif evento.type == KEYDOWN:
-            if input_ativo:
-                if evento.key == K_BACKSPACE:
-                    pergunta = pergunta[:-1]
-                elif evento.key == K_RETURN:
-                    resposta = "Você perguntou: " + pergunta
-                    pergunta = ""
-                else:
-                    pergunta += evento.unicode
-        elif evento.type == MOUSEBUTTONDOWN:
-            if pygame.Rect(250, 10, 300, 32).collidepoint(evento.pos):
-                input_ativo = True
-            else:
-                input_ativo = False
-
-    janela.fill(branco)
-
-    # Renderiza a pergunta
-    texto_pergunta = fonte.render("Digite sua pergunta:", True, preto)
-    janela.blit(texto_pergunta, (10, 10))
-    pygame.draw.rect(janela, preto, (250, 10, 300, 32), 2)
-    texto_caixa_pergunta = fonte.render(pergunta, True, preto)
-    janela.blit(texto_caixa_pergunta, (255, 10))
-
-    # Renderiza a resposta
-    if resposta:
-        texto_resposta = fonte.render(resposta, True, preto)
-        janela.blit(texto_resposta, (10, 50))
-
-    pygame.display.flip()
-
-pygame.quit()
+for num in inteiro,decimal:
+    for i in num:
+        print(int(i))
